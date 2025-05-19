a3c1388b785a2d7f82734fc0025bc7d2627fda50
akshat-angelone <akshat.asthana@angelbroking.com>
2025-05-19 17:24:04 +0530

    Calculate shortest distance of all nodes from any node in undirected graph

---
+++ graphs/shortest_distances_bfs.py
@@ -0,0 +1,25 @@
+from build_graph import UndirectedGraph
+from collections import deque
+
+
+def bfs(start:int, n:int, g:dict) -> list:
+    dist = [0] * n
+    q = deque([start])
+    visited = [0] * n
+    visited[start] = 1
+    while q:
+        node = q.popleft()
+        for adj_node in g[node]:
+            if not visited[adj_node]:
+                dist[adj_node] = dist[node] + 1
+                q.append(adj_node)
+                visited[adj_node] = 1
+    return dist
+
+if __name__ == '__main__':
+    G = UndirectedGraph().build_graph()
+    n = G.get_number_nodes()
+    g = G.get_graph()
+    start = int(input("enter starting node: "))
+    distances = bfs(start, n, g)
+    print(distances)
