ff1a2a64207867f4d141bc523f33494c58dd9cab
akshat-angelone <akshat.asthana@angelbroking.com>
2025-05-21 00:34:34 +0530

    Add support to find shortest path

--- graphs/dijkstra_algorithm.py
+++ graphs/dijkstra_algorithm.py
@@ -1,6 +1,7 @@
 import heapq
 import math
 from build_graph import UndirectedGraph, DirectedGraph
+from collections import deque
 
 # NOTE Dijktstra is not applicable for negative weights it leads to infinite loop becoz we'll keep on getting higher
 # negative values continuously.
@@ -18,7 +19,7 @@ def dijkstra(start: int, graph: dict, n: int) -> list:
     dist = [math.inf] * n
     dist[start] = 0
     min_heap = [(0, start)]
-
+    prev_node = [-1]*n
     while min_heap:
         current_distance, current_node = heapq.heappop(min_heap)
         # Not strictly necessary to use a `visited` set here:
@@ -29,9 +30,12 @@ def dijkstra(start: int, graph: dict, n: int) -> list:
             distance = current_distance + weight
             if distance < dist[neighbor]:
                 dist[neighbor] = distance
+                prev_node[neighbor] = current_node
                 heapq.heappush(min_heap, (distance, neighbor))
 
-    return dist
+    return dist, prev_node
+
+
 
 if __name__ == '__main__':
     G = UndirectedGraph(weighted = True)
@@ -39,5 +43,3 @@ if __name__ == '__main__':
     n = G.get_number_nodes()
     sn = int(input("Enter the start node: "))
     print(dijkstra(sn, g, n))
-
-
