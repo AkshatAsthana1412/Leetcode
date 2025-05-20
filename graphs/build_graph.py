108e7bf05774c7637df6cdbd7953fe93773f8d22
akshat-angelone <akshat.asthana@angelbroking.com>
2025-05-20 20:01:51 +0530

    Add logic to build graph from csv file, add support for weighted graphs

--- graphs/build_graph.py
+++ graphs/build_graph.py
@@ -1,10 +1,12 @@
 from collections import defaultdict
+import csv
 
 class Graph():
-    def __init__(self, directed=False):
+    def __init__(self, directed=False, weighted=False):
         self.g = defaultdict(list)
         self.n = 0
         self.directed = directed
+        self.weighted = weighted
 
     def __str__(self):
         return f"{self.g}"
@@ -12,32 +14,62 @@ class Graph():
     def get_graph(self):
         return self.g
     
-    def build_graph(self):
+    def build_graph(self, use_csv=False, file_path=None):
+        if use_csv and file_path:
+            return self.build_graph_from_csv(file_path)
+        
         self.n = int(input("Enter the number of nodes: "))
         m = int(input("Enter the number of edges: "))
         for _ in range(m):
-            k, v = list(map(int, input("Enter the edge: ").split()))
-            self.g[k].append(v)
-            if not self.directed:   
-                self.g[v].append(k)
+            if self.weighted:
+                k, v, w = map(int, input("Enter the edge and weight (u v w): ").split())
+                self.g[k].append((v, w))
+                if not self.directed:
+                    self.g[v].append((k, w))
+            else:
+                k, v = map(int, input("Enter the edge (u v): ").split())
+                self.g[k].append(v)
+                if not self.directed:
+                    self.g[v].append(k)
+        return self
+
+    def build_graph_from_csv(self, file_path):
+        with open(file_path, newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            header = next(reader)
+            self.n = int(header[0])  # Assume first row is "num_nodes"
+            for row in reader:
+                if self.weighted:
+                    k, v, w = map(int, row)
+                    self.g[k].append((v, w))
+                    if not self.directed:
+                        self.g[v].append((k, w))
+                else:
+                    k, v = map(int, row)
+                    self.g[k].append(v)
+                    if not self.directed:
+                        self.g[v].append(k)
         return self
     
     def get_number_nodes(self):
         return self.n
     
 class DirectedGraph(Graph):
-    def __init__(self):
-        super().__init__(directed=True)
+    def __init__(self, weighted=False):
+        super().__init__(directed=True, weighted=weighted)
 
     def reverse_graph(self):
         g_new = defaultdict(list)
         for node in self.g:
-            for adj_node in self.g[node]:
-                g_new[adj_node].append(node)
+            for adj in self.g[node]:
+                if self.weighted:
+                    v, w = adj
+                    g_new[v].append((node, w))
+                else:
+                    g_new[adj].append(node)
         self.g = g_new
         return self
-        
-class UndirectedGraph(Graph):
-    def __init__(self):
-        super().__init__(directed=False)
     
+class UndirectedGraph(Graph):
+    def __init__(self, weighted=False):
+        super().__init__(directed=False, weighted=weighted)
