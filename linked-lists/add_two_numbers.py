131f4aa83afbcfc012c8bef7ce436d2961d05e83
AkshatAsthana1412 <akshatsagittarius96@gmail.com>
2025-05-22 13:42:25 +0530

    Add two numbers linked list

--- linked-lists/add_two_numbers.py
+++ linked-lists/add_two_numbers.py
@@ -16,8 +16,10 @@ class Solution:
             total_sum = 0
             if l1:
                 total_sum += l1.val
+                l1 = l1.next
             if l2:
                 total_sum += l2.val
+                l2 = l2.next
             total_sum += carry
             carry = total_sum // 10
             l3.next = ListNode(total_sum % 10)
