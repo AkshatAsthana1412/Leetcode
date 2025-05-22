494283117763303d302a3c72ac67a58dc3d4d022
AkshatAsthana1412 <akshatsagittarius96@gmail.com>
2025-05-22 17:11:14 +0530

    Backtracking problems

--- recursion/print_subsequences.py
+++ recursion/print_subsequences.py
@@ -7,6 +7,31 @@ def print_subsequences(alist, n, i=0, arr=[]):
     arr.pop()
     print_subsequences(alist, n, i+1, arr)
 
+def print_unique_subsequences(alist, n, i=0, arr=[], seen=set()):
+    if i >= n:
+        if tuple(arr) not in seen:
+            print(arr)
+            seen.add(tuple(arr))
+        return
+    arr.append(alist[i])
+    print_unique_subsequences(alist, n, i+1, arr, seen)
+    arr.pop()
+    print_unique_subsequences(alist, n, i+1, arr, seen)
+
+# def print_unique_subsequences(alist, n, i=0, arr=[]):
+#     if i >= n:
+#         print(arr)
+#         return
+#     if alist[i] in arr:
+#         return
+#     arr.append(alist[i])
+#     print_unique_subsequences(alist, n, i+1, arr)
+#     arr.pop()
+#     print_unique_subsequences(alist, n, i+1, arr)
+
 if __name__ == '__main__':
     l = list(map(int, input("enter the array elements: ").split()))
-    print_subsequences(l, len(l))
+    print("printing all subsequences:")
+    # print_subsequences(l, len(l))
+    print("printing unique subsequences:")
+    print_unique_subsequences(l, len(l))
--- recursion/subsequence_sum.py
+++ recursion/subsequence_sum.py
@@ -2,19 +2,33 @@ def subsequence_sum(alist, n, k, i=0, sum = 0, arr = []):
     if i >= n:
         if sum == k:
             print(arr)
-            return True
-        else:
-            return False
+        return
     arr.append(alist[i])
     sum += alist[i]
-    if subsequence_sum(alist, n, k, i+1, sum, arr):
+    subsequence_sum(alist, n, k, i+1, sum, arr)
+    sum -= arr.pop()
+    subsequence_sum(alist, n, k, i+1, sum, arr)
+
+def print_one_subsequence(alist, n, k, arr=[], i=0, sum=0):
+    if i >= n:
+        if sum == k:
+            print(arr)
+            return True
+        return False
+    arr.append(alist[i])
+    sum += alist[i]
+    if print_one_subsequence(alist, n, k, arr, i+1, sum):
         return True
     sum -= arr.pop()
-    if subsequence_sum(alist, n, k, i+1, sum, arr):
+    if print_one_subsequence(alist, n, k, arr, i+1, sum):
         return True
     return False
+
+    
 
 if __name__ == '__main__':
     l = list(map(int, input("enter the array elements: ").split()))
     k = int(input("Enter the desired sum: "))
     subsequence_sum(l, len(l), k)
+    print("Printing any one subsequence matching this condition: ")
+    print_one_subsequence(l, len(l), k)
