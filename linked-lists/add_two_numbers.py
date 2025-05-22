288498de1963ec4ecf117bcf57ed0782343d0eeb
AkshatAsthana1412 <akshatsagittarius96@gmail.com>
2025-05-22 13:41:51 +0530

    Correct logic for add_two_numbers

--- linked-lists/add_two_numbers.py
+++ linked-lists/add_two_numbers.py
@@ -8,28 +8,25 @@ class ListNode:
 # THE TRICKY PART IN THIS PROBLEM WAS THE NESTED LINKED LIST, traverse it recursively.
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-                
-        def create_num(node):
-            if node is None:
-                return 0
-            return node.val + 10 * create_num(node.next)
-        
-        def create_list(num):
-            if num == 0:
-                return None
-            d = num % 10
-            node = ListNode(d)
-            node.next = create_list(num//10)
-            return node
+        # total_sum = 0
+        carry = 0
+        dummy = ListNode()
+        l3 = dummy
+        while l1 or l2:
+            total_sum = 0
+            if l1:
+                total_sum += l1.val
+            if l2:
+                total_sum += l2.val
+            total_sum += carry
+            carry = total_sum // 10
+            l3.next = ListNode(total_sum % 10)
+            l3 = l3.next
+        if carry:
+            l3.next = ListNode(carry)
+        return dummy.next
 
-        n1 = create_num(l1)
-        n2 = create_num(l2)
-        n3 = n1 + n2
-        if n3 == 0:
-            return ListNode(0)
-        l3 = create_list(n3)
-        
-        return l3
+            
             
 
     
