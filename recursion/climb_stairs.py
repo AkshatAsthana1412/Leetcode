791b74c0e50c962b29d8af1a7b3f8465f6671e93
AkshatAsthana1412 <akshatsagittarius96@gmail.com>
2025-05-24 00:38:29 +0530

    Refactor

--- recursion/climb_stairs.py
+++ recursion/climb_stairs.py
@@ -1,10 +1,10 @@
-def climb_stairs(final_state, cnt=0):
+def climb_stairs(final_state):
     if final_state < 0:
         return 0
     if final_state == 0:
         return 1
-    l = climb_stairs(final_state-1, cnt)
-    r = climb_stairs(final_state-2, cnt)
+    l = climb_stairs(final_state-1)
+    r = climb_stairs(final_state-2)
     return  l+r 
 
 if __name__ == '__main__':
