be8ae6ea574d65a5a1923d84b03e54116e9209b8
AkshatAsthana1412 <akshatsagittarius96@gmail.com>
2025-05-24 23:18:09 +0530

    Connected components in graph

---
+++ graphs/connected_components.py
@@ -0,0 +1,22 @@
+from build_graph import UndirectedGraph
+def dfs(start:int, graph:dict, visited:set=set()) -> list:
+    visited.add(start)
+    for neighbor in graph[start]:
+        if neighbor not in visited:
+            dfs(neighbor, graph, visited)
+    return visited
+
+def connected_components(g:dict, n:int):
+    cnt = 0
+    visited = set()
+    for node in range(n):
+        if node not in visited:
+            cnt += 1
+            visited = dfs(node, g, visited)
+    return cnt
+
+if __name__ == '__main__':
+    G = UndirectedGraph()
+    g = G.build_graph().get_graph()
+    n = G.get_number_nodes()
+    print(f"Number of connected components: {connected_components(g, n)}")
