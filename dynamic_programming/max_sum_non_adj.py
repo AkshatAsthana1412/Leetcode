f1a960892e29bd73759582c6e4ca2e926bf20440
AkshatAsthana1412 <akshatsagittarius96@gmail.com>
2025-05-25 20:07:23 +0530

    Optimise recursion with memoization

--- dynamic_programming/max_sum_non_adj.py
+++ dynamic_programming/max_sum_non_adj.py
@@ -1,5 +1,5 @@
 # O(2^n) this is pure recursive solution
-def max_sum_of_non_adjacent(arr, indx):
+def max_sum_of_non_adjacent(arr, indx, dp):
     if not arr:
         return None
     if indx == 0:
@@ -8,11 +8,15 @@ def max_sum_of_non_adjacent(arr, indx):
     if indx < 0:
         # possible when I picked 1 in the last call instead of 2. So I can only pick 1 - 2 = -1 indx, which is not possible
         return 0
-    max_sum_with_pick = arr[indx] + max_sum_of_non_adjacent(arr, indx - 2)
-    max_sum_without_pick = max_sum_of_non_adjacent(arr, indx - 1)
-    return max(max_sum_with_pick, max_sum_without_pick)
+    if dp[indx] != -1:
+        return dp[indx]
+    max_sum_with_pick = arr[indx] + max_sum_of_non_adjacent(arr, indx - 2, dp)
+    max_sum_without_pick = max_sum_of_non_adjacent(arr, indx - 1, dp)
+    dp[indx] = max(max_sum_with_pick, max_sum_without_pick)
+    return dp[indx]
 
 if __name__ == '__main__':
     arr = list(map(int, input("Enter list elements: ").split()))
     n = len(arr)
-    print(max_sum_of_non_adjacent(arr, n-1))
+    dp = [-1]*n
+    print(max_sum_of_non_adjacent(arr, n-1, dp))
