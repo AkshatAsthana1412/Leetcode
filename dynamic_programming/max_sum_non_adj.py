7012ca58f021b94eaa96a57409688fcd8952748e
AkshatAsthana1412 <akshatsagittarius96@gmail.com>
2025-05-25 20:04:06 +0530

    Max sum of non adjacent elements by top down recursion

---
+++ dynamic_programming/max_sum_non_adj.py
@@ -0,0 +1,18 @@
+# O(2^n) this is pure recursive solution
+def max_sum_of_non_adjacent(arr, indx):
+    if not arr:
+        return None
+    if indx == 0:
+        # if recursion reaches here, that means I picked 2 in the last recursion call
+        return arr[indx]
+    if indx < 0:
+        # possible when I picked 1 in the last call instead of 2. So I can only pick 1 - 2 = -1 indx, which is not possible
+        return 0
+    max_sum_with_pick = arr[indx] + max_sum_of_non_adjacent(arr, indx - 2)
+    max_sum_without_pick = max_sum_of_non_adjacent(arr, indx - 1)
+    return max(max_sum_with_pick, max_sum_without_pick)
+
+if __name__ == '__main__':
+    arr = list(map(int, input("Enter list elements: ").split()))
+    n = len(arr)
+    print(max_sum_of_non_adjacent(arr, n-1))
