0866cdb3b17560fbcf934ab01a063aeac01b2635
AkshatAsthana1412 <akshatsagittarius96@gmail.com>
2025-06-01 19:45:55 +0530

    Number of time array is rotated

---
+++ binary_search/rotated_array4.py
@@ -0,0 +1,25 @@
+# O(n)
+import math
+def rotated_array(arr):
+    l, u = 0, len(arr) - 1
+    ind = 0
+    num = math.inf
+    while l <= u:
+        m = (l+u)//2
+        if arr[m] <= arr[u]:
+            if arr[m] <= num:
+                num = arr[m]
+                ind = m
+            u = m-1
+        else:
+            if arr[l] <= num:
+                num = arr[l]
+                ind = l
+            l = m+1
+    return ind, num
+
+if __name__ == '__main__':
+    arr = list(map(int, input("Enter the array elements: ").split()))
+    ind, num = rotated_array(arr)
+    print(f"Number of times array needs to be rotated: {ind}")
+
